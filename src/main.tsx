b1cb428e2bfa16f16e4f034e92e0761f8f841d5c
tigawanna <denniskinuthiaw@gamil.com>
2023-07-30 17:11:14 +0300

    added dependacies + config

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,37 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "@tanstack/router";
 import { router } from "./pages/routes/router.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; 
+import { QueryClient, QueryClientProvider,MutationCache } from "@tanstack/react-query";
+
+
+export const queryClient: QueryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onSuccess: async (data, variable, context, mutation) => {
+      if (Array.isArray(mutation.meta?.invalidates)) {
+        return queryClient.invalidateQueries({
+          queryKey: mutation.meta?.invalidates,
+        });
+      }
+    },
+  }),
+
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
