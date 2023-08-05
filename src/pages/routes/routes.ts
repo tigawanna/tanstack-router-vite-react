39129d6b909242e75157b83bd70d5eff918aac05
tigawanna <denniskinuthiaw@gamil.com>
2023-08-05 23:46:43 +0300

    add tremor dashboard ui

--- src/pages/routes/routes.ts
+++ src/pages/routes/routes.ts
@@ -3,11 +3,16 @@ import { authRoute } from "../auth/config";
 import { homeRoute } from "../home/config";
 import { postsRoute } from "../posts/config";
 import { profileRoute } from "../profile/config";
+import { aboutRoute } from "@/pages/about/config";
 
+import { aboutRoute } from "@/pages/about/config";
 // ADD NEW IMPORT HERE
 
 // START
 export const routes = [
+aboutRoute,
+aboutRoute,
+aboutRoute,
   homeRoute,
   profileRoute,
   authRoute,
