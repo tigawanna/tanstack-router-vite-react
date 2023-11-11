0aaf2c52716e99ca45c3c4d9f6254a838ad9b68e
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-11-11 11:29:15 +0300

    add admin route

--- src/pages/routes/routes.ts
+++ src/pages/routes/routes.ts
@@ -1,9 +1,8 @@
 import { homeRoute } from "../home/config";
 import { postsRoute } from "../posts/PostLayout";
-import { profileRoute } from "../profile/config";
 import { welcomeRoute } from "@/pages/welcome/config";
 // ADD NEW IMPORT HERE
 
 // START
-export const routes = [welcomeRoute, homeRoute, profileRoute, postsRoute];
+export const routes = [welcomeRoute, homeRoute, postsRoute];
 // END
--- src/pages/welcome/WelcomeLayout.tsx
+++ src/pages/welcome/WelcomeLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Link } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 
 interface WelcomeLayoutProps {}
 
